dab8b7a43509bf517361cc66f54447d1b07b5cc9
Sarbesh1234 <sankarsarbesh@gmail.com>
2025-11-24 16:50:35 -0800

    added main stuff

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from search_algos import forward_selection
 
 def main():
-  num_features = 4
   
-  # create a list of fake features which is just 0 - num_features
+  print("Welcome to Justin Albert, Vinden Drummond, and Sarbesh Sankar's Feature Selection Algorithm.")
+
+  num_features = int(input("Please enter total number of features: "))
+
+  print("Type the number of the algorithm you want to run.")
+  print("\t 1)Forward Selection")
+  print("\t 2)Backward Elimination")
+
+  algo_choice = int(input())
+
   features = list(range(num_features))
-  print(forward_selection(features))
-  return 0
+
+  if algo_choice == 1:
+    print(forward_selection(features))
+  elif algo_choice == 2:
+    print(backward_elimination(features))
+  else:
+    print("Invalid choice")
+    return 0
+
 
 main()
