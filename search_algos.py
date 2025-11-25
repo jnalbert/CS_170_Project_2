b50b5a085dd14ed01cd12eb54d02279d07ebbf66
VindenD <vinden.drummond@gmail.com>
2025-11-24 16:52:50 -0800

    backward elimination

--- search_algos.py
+++ search_algos.py
@@ -83,3 +83,75 @@ def forward_selection(features):
     )
 
     return curr_best_features
+
+def backward_elimination(features):
+    # start with all features
+    all_features_accuracy = evaluation_function(features)
+    print(
+        'Using all features and "random" evaluation, I get an accuracy of '
+        + str(round(all_features_accuracy * 100, 1))
+        + "%"
+    )
+    print("\nBeginning search.\n")
+    # make a copy so original list isnt modified
+    curr_best_features = features.copy()
+    curr_best_accuracy = all_features_accuracy
+
+    # while we have more than one feature, try removing one at a time
+    while len(curr_best_features) > 1:
+        best_feature_to_remove = None
+        best_accuracy = -1
+        best_set = None
+
+        # try removing each feature that is in the list 
+        for feature in curr_best_features:
+            new_feature_set = curr_best_features.copy()
+            new_feature_set.remove(feature)
+
+            accuracy = evaluation_function(new_feature_set)
+
+            feature_str = format_feature_set(new_feature_set)
+            print(
+                "Using feature(s) "
+                + feature_str
+                + " accuracy is "
+                + str(round(accuracy * 100, 1))
+                + "%"
+            )
+
+            # after removing one feature, keep track of best accuracy subset
+            if accuracy > best_accuracy:
+                best_accuracy = accuracy
+                best_feature_to_remove = feature
+                best_set = new_feature_set
+
+        # if removing the feature decreases the acurracy then stop
+        if best_accuracy < curr_best_accuracy:
+            print("\n(Warning, Accuracy has decreased!)\n")
+            break
+
+        # otherwise, update the current best subset
+        curr_best_features = best_set
+        curr_best_accuracy = best_accuracy
+
+        feature_str = format_feature_set(curr_best_features)
+        print(
+            "Removing " + str(best_feature_to_remove) +
+            ". Feature set "
+            + feature_str
+            + " was best, accuracy is "
+            + str(round(curr_best_accuracy * 100, 1))
+            + "%"
+        )
+        print()
+
+    final_feature_str = format_feature_set(curr_best_features)
+    print(
+        "Finished search!! The best feature subset is "
+        + final_feature_str
+        + ", which has an accuracy of "
+        + str(round(curr_best_accuracy * 100, 1))
+        + "%"
+    )
+
+    return curr_best_features
