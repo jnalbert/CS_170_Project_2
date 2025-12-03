a4b380a5fd1024b3bcc74c471cd15365d4384e40
Justin Albert <jnalbert879@gmail.com>
2025-12-02 23:13:24 -0800

    add trace printing for part 2

--- main.py
+++ main.py
@@ -1,24 +1,72 @@
+import time
 from search_algos import forward_selection
 from search_algos import backward_elimination
+from validator import Validator
+from nn_classifier import NearestNeighborClassifier
+# def main():
+  
+#   print("Welcome to Justin Albert, Vinden Drummond, and Sarbesh Sankar's Feature Selection Algorithm.")
+
+#   num_features = int(input("Please enter total number of features: "))
+
+#   print("Type the number of the algorithm you want to run.")
+#   print("\t 1)Forward Selection")
+#   print("\t 2)Backward Elimination")
+
+#   algo_choice = int(input())
+
+#   features = list(range(num_features))
+
+#   if algo_choice == 1:
+#     print(forward_selection(features))
+#   elif algo_choice == 2:
+#     print(backward_elimination(features))
+
+def run_classifier_and_validator(dataset, feature_subset):
+  classifier = NearestNeighborClassifier()
+  validator = Validator()
+  accuracy = validator.validate(feature_subset, classifier, dataset)
+  return accuracy
+
+# adapted from https://stackoverflow.com/questions/29307532/python-how-can-read-as-float-numbers-a-series-of-strings-from-a-text-file
+def load_dataset(filename):
+  print("Loading dataset from " + filename + "...")
+  start_time = time.time()
+  with open(filename, 'r') as file:
+    lines = file.readlines()
+    dataset = []
+    for line in lines:
+      line = line.strip()
+      if line:
+        features = []
+        for x in line.split():
+            features.append(float(x))
+        dataset.append((features[0], features[1:]))
+  print("Time taken: " + str(round((time.time() - start_time) * 1000, 2)) + " ms\n")
+  return dataset
 
 def main():
+  print("Welcome to Justin Albert, Vinden Drummond, and Sarbesh Sankar's NN classifier and validator.")
   
-  print("Welcome to Justin Albert, Vinden Drummond, and Sarbesh Sankar's Feature Selection Algorithm.")
+  print("Type the number of the dataset you want to run.")
+  print("\t 1) Small Test Dataset")
+  print("\t 2) Large Test Dataset")
 
-  num_features = int(input("Please enter total number of features: "))
-
-  print("Type the number of the algorithm you want to run.")
-  print("\t 1)Forward Selection")
-  print("\t 2)Backward Elimination")
-
-  algo_choice = int(input())
-
-  features = list(range(num_features))
-
-  if algo_choice == 1:
-    print(forward_selection(features))
-  elif algo_choice == 2:
-    print(backward_elimination(features))
+  dataset_choice = int(input())
+  dataset = None
+  # a 1 indexed array of the features to use
+  feature_subset = []
+  if dataset_choice == 1:
+    dataset = load_dataset("small-test-dataset.txt")
+    feature_subset = [3, 5, 7]
+  elif dataset_choice == 2:
+    dataset = load_dataset("large-test-dataset.txt")
+    feature_subset = [1, 15, 27]
+  
+  print("Running NN classifier and validator on dataset...\n")
+  # print(dataset)
+  accuracy = run_classifier_and_validator(dataset, feature_subset)
+  
 
 
 main()
--- validator.py
+++ validator.py
@@ -1,39 +1,60 @@
+import time
+
+
 class Validator:
+    # dataset is a list of arrays where (class_lable, feature_vector)
+    # feature_subset is a list of feature indexes
     def validate(self, feature_subset, classifier, dataset):
+        print("Validating with feature subset: {"+ ", ".join(map(str, feature_subset))+ "}\n")
         # leave-one-out cross-validation
         correct_predictions = 0
         total_instances = len(dataset)
-        
+
+        start_time = time.time()  # Record start time in milliseconds
+        start_time_50 = time.time() * 1000
+
         for i in range(total_instances):
             test_instance = dataset[i]
             test_class = test_instance[0]
-            
+
             # use all instances except i for training
             training_data = []
             for j in range(total_instances):
                 if j != i:
                     training_data.append(dataset[j])
-            
+
             # extract features and convert to (feature_vector, class_label) format
             training_tuples = []
             for instance in training_data:
                 class_label = instance[0]
                 feature_vector = []
-                for feat_idx in feature_subset: 
-                    feature_vector.append(instance[feat_idx])
+                for feat_idx in feature_subset:
+                    feature_vector.append(instance[1][feat_idx - 1])
                 training_tuples.append((feature_vector, class_label))
-            
+
             classifier.train(training_tuples)
-            
+
             # extract features from test instance
             test_features = []
             for feat_idx in feature_subset:
-                test_features.append(test_instance[feat_idx])
-            
+                test_features.append(test_instance[1][feat_idx - 1])
+
             predicted_class = classifier.test(test_features)
-            
+
             if predicted_class == test_class:
                 correct_predictions += 1
-        
-        return correct_predictions / total_instances
 
+            # print the time every 50 rows
+            if (i + 1) % 50 == 0:
+                end_time_50 = time.time() * 1000
+                batch_time = end_time_50 - start_time_50
+                print("Time for rows " + str((i - 48)) + " to " + str(i + 1) + " - " + str(round(batch_time, 2)) + " ms")
+                start_time_50 = time.time() * 1000
+
+        print("\n")
+        print("Correct predictions: " + str(correct_predictions))
+        print("Total instances: " + str(total_instances))
+        print("Accuracy: " + str(correct_predictions / total_instances * 100) + "%")
+        print("Total time taken: " + str(round((time.time() - start_time) * 1000, 2)) + " ms")
+
+        return correct_predictions / total_instances
